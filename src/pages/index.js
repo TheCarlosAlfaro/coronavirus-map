32fd99b32fe27e8307d89694cc4a888a83360a95
devbysalas <rgkalin@gmail.com>
2020-04-04 14:27:56 -0700

    Adds axios and fethes the coronavirus data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
-import React, from 'react';
+import React from 'react';
 import Helmet from 'react-helmet';
 import L from 'leaflet';
 
-import from 'lib/map';
+// import from 'lib/map';
 
 import Layout from 'components/Layout';
 import Container from 'components/Container';
 import Map from 'components/Map';
 
+import axios from 'axios';
 
 const LOCATION = {
   lat: 38.9072,
@@ -16,19 +17,26 @@ const LOCATION = {
 const CENTER = [LOCATION.lat, LOCATION.lng];
 const DEFAULT_ZOOM = 2;
 
-
-
-
 const IndexPage = () => {
-
-
   /**
    * mapEffect
    * @description Fires a callback once the page renders
    * @example Here this is and example of being used to zoom in and set a popup on load
    */
 
-  async function mapEffect({ leafletElement: map } = {}) {}
+  async function mapEffect({ leafletElement: map } = {}) {
+    let response;
+
+    try {
+      response = await axios.get('https://corona.lmao.ninja/countries');
+    } catch (error) {
+      console.log(`Failed to fetch countries: ${error.message}`, error);
+      return;
+    }
+
+    const { data = [] } = response;
+    console.log(data);
+  }
 
   const mapSettings = {
     center: CENTER,
